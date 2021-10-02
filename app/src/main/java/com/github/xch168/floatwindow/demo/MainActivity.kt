e03ae9b1826140815f4e642d271ed83f12a5a549
Kemp Xu <kemp.xu@foxmail.com>
2021-10-02 21:16:47 +0800

    Init commit

---
+++ app/src/main/java/com/github/xch168/floatwindow/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.xch168.floatwindow.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.xch168.floatwindow.FloatWindow
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ library/src/main/java/com/github/xch168/floatwindow/FloatWindow.kt
@@ -0,0 +1,8 @@
+package com.github.xch168.floatwindow
+
+/**
+ * Created by Kemp Xu on 2021/10/2.
+ */
+object FloatWindow {
+
+}
